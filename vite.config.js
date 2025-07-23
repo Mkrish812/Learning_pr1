8851cedaf6860da5b9637d7e27559264964da458
Mkrish812 <mkris812.0@gmail.com>
2025-07-23 20:55:42 +0530

    second First commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Learning_pr1/',
+  base: '/Learning_pr1/',
   plugins: [react(),tailwindcss()]
 })
